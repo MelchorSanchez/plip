6d22a2698fac27ae21decae5d1432efac743df0e
Melchor S <meltxor.sanchez@gmail.com>
2021-05-10 23:45:38 +0200

    Update config.py

--- plip/basic/config.py
+++ plip/basic/config.py
@@ -34,18 +34,18 @@ MODEL = 1  # The model to be selected for multi-model structures (default = 1).
 # Set thresholds for detection of interactions
 
 # Thresholds for detection (global variables)
-BS_DIST = 7.5  # Determines maximum distance to include binding site residues
+BS_DIST = 10 #7.5  # Determines maximum distance to include binding site residues
 AROMATIC_PLANARITY = 5.0  # Determines allowed deviation from planarity in aromatic rings
 MIN_DIST = 0.5  # Minimum distance for all distance thresholds
 # Some distance thresholds were extended (max. 1.0A) if too restrictive too account for low-quality structures
 HYDROPH_DIST_MAX = 4.0  # Distance cutoff for detection of hydrophobic contacts
-HBOND_DIST_MAX = 4.1  # Max. distance between hydrogen bond donor and acceptor (Hubbard & Haider, 2001) + 0.6 A
+HBOND_DIST_MAX = 3.5 #4.1  # Max. distance between hydrogen bond donor and acceptor (Hubbard & Haider, 2001) + 0.6 A
 HBOND_DON_ANGLE_MIN = 100  # Min. angle at the hydrogen bond donor (Hubbard & Haider, 2001) + 10
-PISTACK_DIST_MAX = 5.5  # Max. distance for parallel or offset pistacking (McGaughey, 1998)
+PISTACK_DIST_MAX = 6.0 #5.5  # Max. distance for parallel or offset pistacking (McGaughey, 1998)
 PISTACK_ANG_DEV = 30  # Max. Deviation from parallel or perpendicular orientation (in degrees)
 PISTACK_OFFSET_MAX = 2.0  # Maximum offset of the two rings (corresponds to the radius of benzene + 0.5 A)
 PICATION_DIST_MAX = 6.0  # Max. distance between charged atom and aromatic ring center (Gallivan and Dougherty, 1999)
-SALTBRIDGE_DIST_MAX = 5.5  # Max. distance between centers of charge for salt bridges (Barlow and Thornton, 1983) + 1.5
+SALTBRIDGE_DIST_MAX = 4.0 # 5.5  # Max. distance between centers of charge for salt bridges (Barlow and Thornton, 1983) + 1.5
 HALOGEN_DIST_MAX = 4.0  # Max. distance between oxy. and halogen (Halogen bonds in biological molecules., Auffinger)+0.5
 HALOGEN_ACC_ANGLE = 120  # Optimal acceptor angle (Halogen bonds in biological molecules., Auffinger)
 HALOGEN_DON_ANGLE = 165  # Optimal donor angle (Halogen bonds in biological molecules., Auffinger)
